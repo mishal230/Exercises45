618642b9bf2466f1acc66b7cb175b6ed9e882baf
mishalzubair <160513180+mishal230@users.noreply.github.com>
2024-04-02 17:15:41 +0500

    Update app.js

--- Exercise_3/app.js
+++ Exercise_3/app.js
@@ -2,4 +2,4 @@
 var personName = "Mishal Zubair";
 console.log("lowerCase:", personName.toLowerCase());
 console.log("UpperCase:", personName.toUpperCase());
-console.log("TitleCase:", personName.replace(/\w/g, function (char) { return char.toUpperCase(); }));
+console.log("TitleCase:", personName.replace(/\b\w/g, function (char) { return char.toUpperCase(); }));
