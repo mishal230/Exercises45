110084e68aa0189c97743cc3c56436f51e6ca729
mishalzubair <160513180+mishal230@users.noreply.github.com>
2024-04-03 18:08:48 +0500

    Add files via upload
    correcting console message

--- Exercise_29/app.js
+++ Exercise_29/app.js
@@ -4,10 +4,10 @@ if (favorite_fruits.includes('Apple')) {
     console.log("You really like Apple!");
 }
 if (favorite_fruits.includes('Mango')) {
-    console.log("You really like Apple!");
+    console.log("You really like Mango!");
 }
 if (favorite_fruits.includes('Banana')) {
-    console.log("You really like Apple!");
+    console.log("You really like Banana!");
 }
 if (favorite_fruits.includes('Grapes')) {
     console.log("You really like Grapes!");
--- Exercise_29/app.ts
+++ Exercise_29/app.ts
@@ -4,11 +4,11 @@ if(favorite_fruits.includes('Apple')){
 } 
 
 if(favorite_fruits.includes('Mango')){
-    console.log("You really like Apple!");
+    console.log("You really like Mango!");
 } 
 
 if(favorite_fruits.includes('Banana')){
-    console.log("You really like Apple!");
+    console.log("You really like Banana!");
 } 
 
 if(favorite_fruits.includes('Grapes')){
